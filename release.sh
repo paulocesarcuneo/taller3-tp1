cdd7d6b9a1a9c5db378d1e8f4c335aa9e321208b
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2021-10-04 00:18:15 -0300

    Adds release versioning

--- release.sh
+++ release.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo $(( $(cat public/version) + 1 )) > public/version
+
 zip site.zip -r public visits.py main.py requirements.txt
+
+git add .
+git commit -m "Release $(cat public/version)"
+git tag -a "$(cat public/version)"
